09f087759764fce285d3c384d2c03dcd3c39b236
Jhon Doe <johndoe@example.com>
2023-12-28 15:33:22 -0600

    fix: crons para activar y desactivar clientes

--- src/main/java/com/tutorial/crud/scheduling/ScheduledTasks.java
+++ src/main/java/com/tutorial/crud/scheduling/ScheduledTasks.java
@@ -12,42 +12,30 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
-
-    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
     @Autowired
     private ClienteServiceImpl clienteService;
 
     @Scheduled(cron = "0 0 23 * * *") // Cron expression for running every minute
     public void updateCollectionAlpha2() {
         clienteService.actualizarActivosxClub(6);
-        log.info("Clientes Alpha 2 activos a las {}", dateFormat.format(new Date()));
         clienteService.actualizarEtapasCanceladosxClub(6);
-        log.info("Clientes Alpha 2 bajas y etapas actualizados a las {}", dateFormat.format(new Date()));
     }
 
-    @Scheduled(cron = "0 0 1 * * *") // Cron expression for running every minute
+    @Scheduled(cron = "0 0 2 * * *") // Cron expression for running every minute
     public void updateCollectionAlpha3() {
         clienteService.actualizarActivosxClub(7);
-        log.info("Clientes Alpha 3 activos a las {}", dateFormat.format(new Date()));
         clienteService.actualizarEtapasCanceladosxClub(7);
-        log.info("Clientes Alpha 3 bajas y etapas actualizados a las {}", dateFormat.format(new Date()));
     }
 
-    @Scheduled(cron = "0 0 3 * * *") // Cron expression for running every minute
+    @Scheduled(cron = "0 0 4 * * *") // Cron expression for running every minute
     public void updateCollectionCIMERA() {
         clienteService.actualizarActivosxClub(8);
-        log.info("Clientes CIMERA activos a las {}", dateFormat.format(new Date()));
         clienteService.actualizarEtapasCanceladosxClub(8);
-        log.info("Clientes CIMERA bajas y etapas actualizados a las {}", dateFormat.format(new Date()));
     }
 
-    @Scheduled(cron = "0 0 5 * * *") // Cron expression for running every minute
+    @Scheduled(cron = "0 0 6 * * *") // Cron expression for running every minute
     public void updateCollectionSPORTS() {
         clienteService.actualizarActivosxClub(9);
-        log.info("Clientes Sports Plaza activos a las {}", dateFormat.format(new Date()));
         clienteService.actualizarEtapasCanceladosxClub(9);
-        log.info("Clientes Sports Plaza bajas y etapas actualizados a las {}", dateFormat.format(new Date()));
     }
 }
