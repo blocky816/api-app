8547ae7b7ee11197f7a7abca463f53d5233ed82f
Jhon Doe <johndoe@example.com>
2024-01-17 11:36:01 -0600

    feat: obtener costo QR  por usuario

---
+++ src/main/java/com/tutorial/crud/repository/QREstacionamientoCostoRepository.java
@@ -0,0 +1,7 @@
+package com.tutorial.crud.repository;
+
+import com.tutorial.crud.entity.QREstacionamientoCosto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QREstacionamientoCostoRepository extends JpaRepository<QREstacionamientoCosto, Integer> {
+}
